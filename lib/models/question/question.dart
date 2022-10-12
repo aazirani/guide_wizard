68b581e31190980ebb739050ad423b76a0bf3fc0
Shayan Kebriti <shayankebriti@gmail.com>
2022-10-12 12:29:39 +0330

    Update lib/models/question/question.dart
    Code Cleanup
    
    Co-authored-by: Amin Azirani <aminakbari@live.com>

--- lib/models/question/question.dart
+++ lib/models/question/question.dart
@@ -1,5 +1,5 @@
 abstract class Question {
   late String title, description;
   late List<Map<String, dynamic>> options;
-  late bool multiChoice, isAnswered=false;
+  late bool multiChoice, isAnswered = false;
 }
