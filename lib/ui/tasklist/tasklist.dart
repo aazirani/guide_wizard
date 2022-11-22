67393b053dd7b02f8315644acd729a6625d05ef5
ZohaNsar <39709818+ZohaNsar@users.noreply.github.com>
2022-11-22 17:09:45 +0330

    Add Task Timeline

---
+++ lib/ui/tasklist/tasklist.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import '../../constants/colors.dart';
+import '../tasklist/tasklist_timeline.dart';
+import '../../models/task/task.dart';
+import '../../utils/enums/enum.dart';
+
+class TaskList extends StatefulWidget {
+  const TaskList({Key? key}) : super(key: key);
+
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [
+    Task(
+        title: "Application Dates",
+        deadline: DateTime.now(),
+        status: TaskStatus.notDone),
+    Task(
+        title: "Requirements",
+        deadline: DateTime.now(),
+        status: TaskStatus.notDone),
+    Task(title: "Language Certificate", status: TaskStatus.Done),
+    Task(
+        title: "Private Housing",
+        deadline: DateTime.now(),
+        status: TaskStatus.Done),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: _buildAppBar(),
+      body: _buildBody(tasks),
+    );
+  }
+
+  //appBar methods .............................................................
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      toolbarHeight: _getScreenHeight() / 4,
+      titleSpacing: 5,
+      backgroundColor: AppColors.main_color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(40),
+        ),
+      ),
+      flexibleSpace: SafeArea(
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, top: 10),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: Icon(Icons.arrow_back_ios),
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30.0, top: 60, bottom: 0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("University",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                        Text("4 tasks",
+                            style: TextStyle(fontSize: 15, color: Colors.white))
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: _buildProgressBar()),
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // body methods ..............................................................
+  Widget _buildBody(tasks) {
+    return TaskListTimeLine(tasks: tasks);
+  }
+
+  Widget _buildProgressBar() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Container(
+        // color: Colors.amber,
+        height: 20,
+        width: _getScreenWidth() / 1.19,
+        child: Padding(
+            padding: EdgeInsets.only(right: 0, bottom: 15),
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              child: LinearProgressIndicator(
+                  // minHeight: 4,
+                  value: 0.2,
+                  backgroundColor: Colors.white,
+                  valueColor: AlwaysStoppedAnimation(
+                      Color.fromARGB(255, 47, 205, 144))),
+            )),
+      ),
+    );
+  }
+
+  // general methods ...........................................................
+  double _getScreenHeight() {
+    return MediaQuery.of(context).size.height;
+  }
+
+  double _getScreenWidth() {
+    return MediaQuery.of(context).size.width;
+  }
+}
