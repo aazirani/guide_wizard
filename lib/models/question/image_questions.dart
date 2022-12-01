7a7cecbf57c3339bc10b094fa6c743a3041c87b0
Shayan Kebriti <shayankebriti@gmail.com>
2022-12-01 20:25:21 +0330

    Question model almost done (don't run the app here!)

--- lib/models/question/image_questions.dart
+++ lib/models/question/image_questions.dart
@@ -1,22 +1,22 @@
-import 'package:boilerplate/models/question/question.dart';
-
-class ImageQuestion implements Question{
-  String title, description;
-  List<Map<String, dynamic>> options;
-  bool multiChoice, isAnswered = false;
-  late int columns;
-  double? height, width;
-  ImageQuestion({
-    required this.title,
-    required this.description,
-    required this.options,
-    required this.multiChoice,
-    this.columns = 2,
-    this.height,
-    this.width,
-  });
-
-  void answer(){
-    isAnswered = true;
-  }
-}
+// import 'package:boilerplate/models/question/question.dart';
+//
+// class ImageQuestion implements Question{
+//   String title, description;
+//   List<Map<String, dynamic>> options;
+//   bool multiChoice, isAnswered = false;
+//   late int columns;
+//   double? height, width;
+//   ImageQuestion({
+//     required this.title,
+//     required this.description,
+//     required this.options,
+//     required this.multiChoice,
+//     this.columns = 2,
+//     this.height,
+//     this.width,
+//   });
+//
+//   void answer(){
+//     isAnswered = true;
+//   }
+// }
--- lib/models/question/question.dart
+++ lib/models/question/question.dart
@@ -1,5 +1,121 @@
-abstract class Question {
-  late String title, description;
-  late List<Map<String, dynamic>> options;
-  late bool multiChoice, isAnswered = false;
+// abstract class Question {
+//   late String title, description;
+//   late List<Map<String, dynamic>> options;
+//   late bool multiChoice, isAnswered = false;
+// }
+
+class Question {
+  //Server Values
+  int id;
+  String title;
+  String sub_title;
+  String type;
+  int axis_count;
+  bool is_multiple_choice;
+  List<dynamic> info_url; //TODO
+  List<dynamic> info_description; //TODO
+  int answer_required;
+  bool answers_selected_by_default;
+  int step_id;
+  int creator_id;
+  String created_at;
+  String updated_at;
+  List<dynamic> step; //TODO
+  //List<Answer> answers; //TODO
+
+  Question({
+    required this.id,
+    required this.title,
+    required this.sub_title,
+    required this.type,
+    required this.axis_count,
+    required this.is_multiple_choice,
+    required this.info_url,
+    required this.info_description,
+    required this.answer_required,
+    required this.answers_selected_by_default,
+    required this.step_id,
+    required this.creator_id,
+    required this.created_at,
+    required this.updated_at,
+    required this.step,
+    // required this.answers,
+  });
+
+  factory Question.fromMap(Map<String, dynamic> json) {
+    //for Userfrosting
+    // return Question(
+    //   id: json["id"],
+    //   title: json["title"],
+    //   sub_title: json["sub_title"],
+    //   updated_at: json["updated_at"],
+    //   answers: json["answers"].map((answer) => Answer.fromMap(answer)).toList().cast<Answer>(),
+    //   type: json["type"],
+    //   is_multiple_choice: json["is_multiple_choice"] == 1  ? true : false,
+    //   axis_count: json["axis_count"],
+    //   info_url: json["info_url"],
+    //   info_description: json["info_description"],
+    //   show_on_startup: json["show_on_startup"] == 1  ? true : false,
+    //   answers_selected_by_default: json["answers_selected_by_default"] == 1  ? true : false,
+    // );
+    return Question(
+        id: json["id"],
+        title: json["title"],
+        sub_title: json["sub_title"],
+        type: json["type"],
+        axis_count: json["axis_count"],
+        is_multiple_choice: json["is_multiple_choice"] == 1  ? true : false,
+        info_url: json["info_url"],
+        info_description: json["info_description"],
+        answer_required: json["answer_required"],
+        answers_selected_by_default: json["answers_selected_by_default"] == 1  ? true : false,
+        step_id: json["step_id"],
+        creator_id: json["creator_id"],
+        created_at: json["created_at"],
+        updated_at: json["updated_at"],
+        step: json["step"]
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "title": title,
+      "sub_title": sub_title,
+      "type": type,
+      "axis_count": axis_count,
+      "is_multiple_choice": is_multiple_choice,
+      "info_url": info_url,
+      "info_description": info_description,
+      "answer_required": answer_required,
+      "answers_selected_by_default": answers_selected_by_default,
+      "step_id": step_id,
+      "created_at": created_at,
+      "updated_at": updated_at,
+      "step": step,
+      // "answers": answers,
+    };
+  }
+
+  // Answer getAnswer(int id) {
+  //   for (Answer answer in answers) {
+  //     if (answer.id == id) {
+  //       return answer;
+  //     }
+  //   }
+  //   return null;
+  // }
+
+  // void deselectAllAnswers() {
+  //   for (Answer answer in answers) {
+  //     answer.selected = false;
+  //   }
+  // }
+  //
+  // void selectAnswers(List<Answer> answersToBeSelected) {
+  //   deselectAllAnswers();
+  //   for (Answer answer in answersToBeSelected) {
+  //     answer.selected = true;
+  //   }
+  // }
 }
