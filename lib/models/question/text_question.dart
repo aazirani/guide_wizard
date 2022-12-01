636a1b39f4f876bceef86b20e59f46793d30db0c
Shayan Kebriti <shayankebriti@gmail.com>
2022-12-01 20:25:21 +0330

    Question model almost done (don't run the app here!)

--- lib/models/question/text_question.dart
+++ lib/models/question/text_question.dart
@@ -1,18 +1,18 @@
-import 'package:boilerplate/models/question/question.dart';
-
-class TextQuestion implements Question{
-  String title, description;
-  List<Map<String, dynamic>> options;
-  bool multiChoice, isAnswered = false;
-
-  TextQuestion({
-    required this.title,
-    required this.description,
-    required this.options,
-    required this.multiChoice,
-  });
-
-  void answer(){
-    isAnswered = true;
-  }
-}
+// import 'package:boilerplate/models/question/question.dart';
+//
+// class TextQuestion implements Question{
+//   String title, description;
+//   List<Map<String, dynamic>> options;
+//   bool multiChoice, isAnswered = false;
+//
+//   TextQuestion({
+//     required this.title,
+//     required this.description,
+//     required this.options,
+//     required this.multiChoice,
+//   });
+//
+//   void answer(){
+//     isAnswered = true;
+//   }
+// }
