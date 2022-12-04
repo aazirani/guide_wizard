24768050dc6b34265d5638b6f1c274fd2754be08
Shayan Kebriti <shayankebriti@gmail.com>
2022-12-04 16:07:23 +0330

    task and step updated

--- lib/models/task/task.dart
+++ lib/models/task/task.dart
@@ -1,27 +1,33 @@
 import 'package:boilerplate/models/sub_task/sub_task.dart';
 import 'package:boilerplate/models/title/title.dart';
+import 'package:boilerplate/models/question/question.dart';
 
 class Task {
   int id;
-  Title title;
+  Title text;
+  Title description;
   String type;
-  List<String>? image;
+  String? image1;
+  String? image2;
+  String? fa_icon;
   List<SubTask> sub_tasks;
   String creator_id;
   String created_at;
   String updated_at;
-  int step_id;
+  List<Question> quesions;
 
   Task({
     required this.id,
-    required this.title,
+    required this.text,
+    required this.description,
     required this.type,
-    this.image,
+    required this.image1,
+    required this.image2,
     required this.sub_tasks,
     required this.creator_id,
     required this.created_at,
     required this.updated_at,
-    required this.step_id,
+    required this.quesions,
   });
 
   factory Task.fromMap(Map<String, dynamic> json) {
@@ -30,25 +36,29 @@ class Task {
     //           Endpoints.domain + '/api/app/answers/img/', '')}',
     return Task(
       id: json["id"],
-      title: json["title"].cast<Title>(),
+      text: json["text"].cast<Title>(),
+      description: json["description"].cast<Title>(),
       type: json["type"],
-      image: json["image"].cast<String>(),
+      image1: json["image1"].cast<String>(),
+      image2: json["image2"].cast<String>(),
       sub_tasks: json["sub_tasks"].cast<SubTask>(),
       creator_id: json["creator_id"],
       created_at: json["created_at"],
       updated_at: json["updated_at"],
-      step_id: json["step_id"],
+      quesions: json["question"].cast<Question>(),
     );
   }
 
   Map<String, dynamic> toMap() => {
     "id": id,
-    "title": title,
-    "image": image,
+    "text": text,
+    "image1": image1,
+    "image2": image2,
     "sub_tasks": sub_tasks,
     "creator_id": creator_id,
     "created_at": created_at,
     "updated_at": updated_at,
+    "question": quesions,
   };
 
 }
