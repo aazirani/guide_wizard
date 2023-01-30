7b09c7dc3ccc5712b3fce2f45572eddff2aaa172
ZohaNsar <39709818+ZohaNsar@users.noreply.github.com>
2023-01-31 01:05:58 +0330

    Update repository and data sources

---
+++ lib/data/network/apis/app_data/app_data_api.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:boilerplate/data/network/constants/endpoints.dart';
+import 'package:boilerplate/data/network/dio_client.dart';
+import 'package:boilerplate/data/network/rest_client.dart';
+import 'package:boilerplate/models/step/step_list.dart';
+
+class StepApi {
+  // dio instance
+  final DioClient _dioClient;
+
+  // rest-client instance
+  final RestClient _restClient;
+
+  // injecting dio instance
+  StepApi(this._dioClient, this._restClient);
+
+  /// Returns list of post in response
+  Future<StepList> getSteps() async {
+    try {
+      final res = await _dioClient.get(Endpoints.getAppData);
+      return StepList.fromJson(res["rows"]);
+    } catch (e) {
+      print(e.toString());
+      throw e;
+    }
+  }
+
+}
