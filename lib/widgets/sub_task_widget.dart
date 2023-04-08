69872d1d4f5a91f83dd6e3356f2bfd7bc76d97d3
Shayan Kebriti <shayankebriti@gmail.com>
2023-04-08 18:59:35 +0330

    merging bugs fixed

--- lib/widgets/sub_task_widget.dart
+++ lib/widgets/sub_task_widget.dart
@@ -1,39 +1,24 @@
 import 'package:boilerplate/constants/colors.dart';
 import 'package:boilerplate/constants/dimens.dart';
 import 'package:boilerplate/models/sub_task/sub_task.dart';
-import 'package:boilerplate/stores/technical_name/technical_name_with_translations_store.dart';
 import 'package:boilerplate/widgets/expansion_content.dart';
 import 'package:boilerplate/widgets/app_expansiontile.dart';
 import 'package:boilerplate/widgets/scrolling_overflow_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:provider/provider.dart';
 import 'package:render_metrics/render_metrics.dart';
 
 class SubTaskWidget extends StatefulWidget {
   int index;
   List<SubTask> subTasks;
   RenderParametersManager renderManager;
-  SubTaskWidget({
-    Key? key,
-    required this.index,
-    required this.subTasks,
-    required this.renderManager,
-  }) : super(key: key);
+  SubTaskWidget({Key? key,required this.index , required this.subTasks, required this.renderManager,}) : super(key: key);
 
   @override
   State<SubTaskWidget> createState() => SubTaskWidgetState();
 }
 
-class SubTaskWidgetState extends State<SubTaskWidget>
-    with AutomaticKeepAliveClientMixin {
-  late TechnicalNameWithTranslationsStore _technicalNameWithTranslationsStore;
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _technicalNameWithTranslationsStore =
-        Provider.of<TechnicalNameWithTranslationsStore>(context);
-  }
+class SubTaskWidgetState extends State<SubTaskWidget> with AutomaticKeepAliveClientMixin {
 
   void _runAtExpanding() {
     setState(() {
@@ -46,33 +31,28 @@ class SubTaskWidgetState extends State<SubTaskWidget>
       widget.subTasks[widget.index].toggleExpanded();
     });
   }
-    }
 
-    Widget _buildExpansionContent() {
-    
-    var markdown_id = widget.subTasks[widget.index].markdown.id;
-      return ExpansionContent(renderManager: widget.renderManager, markdown: _technicalNameWithTranslationsStore
-            .getTechnicalNames(markdown_id)!,);
-    }
+  Widget _buildExpansionContent(String markdown) {
+    return ExpansionContent(renderManager: widget.renderManager, markdown: markdown,);
+  }
 
-    Widget _buildAppExpansionTileWidget() {
-    var sub_task_title_id = widget.subTasks[widget.index].title.id;
-      return AppExpansionTile(
-        onExpansionChanged: ((isNewState) {
-          if (isNewState) {
-            _runAtExpanding();
-          }
-        }),
-        maintainState: true,
-        textColor: AppColors.main_color,
-        iconColor: AppColors.main_color,
-        title: ScrollingOverflowText(text: _technicalNameWithTranslationsStore.getTechnicalNames(sub_task_title_id)!, textStyle: TextStyle(fontSize: 19),),
-        key: widget.subTasks[widget.index].globalKey,
-        children: <Widget>[
-          _buildExpansionContent(),
-        ],
-      );
-    }
+  Widget _buildAppExpansionTileWidget() {
+    return AppExpansionTile(
+      onExpansionChanged: ((isNewState) {
+        if (isNewState) {
+          _runAtExpanding();
+        }
+      }),
+      maintainState: true,
+      textColor: AppColors.main_color,
+      iconColor: AppColors.main_color,
+      title: ScrollingOverflowText(text: widget.subTasks[widget.index].title.string, textStyle: TextStyle(fontSize: 19),),
+      key: widget.subTasks[widget.index].globalKey,
+      children: <Widget>[
+        _buildExpansionContent(widget.subTasks[widget.index].markdown.string),
+      ],
+    );
+  }
 
   Widget _buildAppExpansionTileWidgetWithCustomTheme() {
     return ListTileTheme(
@@ -90,6 +70,7 @@ class SubTaskWidgetState extends State<SubTaskWidget>
     );
   }
 
+
   Widget _buildRoundedExpansionTile() {
     return ClipRRect(
       borderRadius: Dimens.expansionTileBorderRadius,
@@ -98,7 +79,8 @@ class SubTaskWidgetState extends State<SubTaskWidget>
           shape: RoundedRectangleBorder(
             borderRadius: Dimens.expansionTileBorderRadius,
           ),
-          child: _buildAppExpansionTileWidgetWithCustomTheme()),
+          child: _buildAppExpansionTileWidgetWithCustomTheme()
+      ),
     );
   }
 
