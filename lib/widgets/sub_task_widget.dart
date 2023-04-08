ca23b6d39baa4db1ffd9fb57e9cdc472a1799a55
ZohaNsar <39709818+ZohaNsar@users.noreply.github.com>
2023-04-08 19:44:18 +0330

    Revert "merging bugs fixed"
    This reverts commit 69872d1d4f5a91f83dd6e3356f2bfd7bc76d97d3.

--- lib/widgets/sub_task_widget.dart
+++ lib/widgets/sub_task_widget.dart
@@ -1,24 +1,39 @@
 import 'package:boilerplate/constants/colors.dart';
 import 'package:boilerplate/constants/dimens.dart';
 import 'package:boilerplate/models/sub_task/sub_task.dart';
+import 'package:boilerplate/stores/technical_name/technical_name_with_translations_store.dart';
 import 'package:boilerplate/widgets/expansion_content.dart';
 import 'package:boilerplate/widgets/app_expansiontile.dart';
 import 'package:boilerplate/widgets/scrolling_overflow_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:provider/provider.dart';
 import 'package:render_metrics/render_metrics.dart';
 
 class SubTaskWidget extends StatefulWidget {
   int index;
   List<SubTask> subTasks;
   RenderParametersManager renderManager;
-  SubTaskWidget({Key? key,required this.index , required this.subTasks, required this.renderManager,}) : super(key: key);
+  SubTaskWidget({
+    Key? key,
+    required this.index,
+    required this.subTasks,
+    required this.renderManager,
+  }) : super(key: key);
 
   @override
   State<SubTaskWidget> createState() => SubTaskWidgetState();
 }
 
-class SubTaskWidgetState extends State<SubTaskWidget> with AutomaticKeepAliveClientMixin {
+class SubTaskWidgetState extends State<SubTaskWidget>
+    with AutomaticKeepAliveClientMixin {
+  late TechnicalNameWithTranslationsStore _technicalNameWithTranslationsStore;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _technicalNameWithTranslationsStore =
+        Provider.of<TechnicalNameWithTranslationsStore>(context);
+  }
 
   void _runAtExpanding() {
     setState(() {
@@ -31,28 +46,33 @@ class SubTaskWidgetState extends State<SubTaskWidget> with AutomaticKeepAliveCli
       widget.subTasks[widget.index].toggleExpanded();
     });
   }
+    }
 
-  Widget _buildExpansionContent(String markdown) {
-    return ExpansionContent(renderManager: widget.renderManager, markdown: markdown,);
-  }
+    Widget _buildExpansionContent() {
+    
+    var markdown_id = widget.subTasks[widget.index].markdown.id;
+      return ExpansionContent(renderManager: widget.renderManager, markdown: _technicalNameWithTranslationsStore
+            .getTechnicalNames(markdown_id)!,);
+    }
 
-  Widget _buildAppExpansionTileWidget() {
-    return AppExpansionTile(
-      onExpansionChanged: ((isNewState) {
-        if (isNewState) {
-          _runAtExpanding();
-        }
-      }),
-      maintainState: true,
-      textColor: AppColors.main_color,
-      iconColor: AppColors.main_color,
-      title: ScrollingOverflowText(text: widget.subTasks[widget.index].title.string, textStyle: TextStyle(fontSize: 19),),
-      key: widget.subTasks[widget.index].globalKey,
-      children: <Widget>[
-        _buildExpansionContent(widget.subTasks[widget.index].markdown.string),
-      ],
-    );
-  }
+    Widget _buildAppExpansionTileWidget() {
+    var sub_task_title_id = widget.subTasks[widget.index].title.id;
+      return AppExpansionTile(
+        onExpansionChanged: ((isNewState) {
+          if (isNewState) {
+            _runAtExpanding();
+          }
+        }),
+        maintainState: true,
+        textColor: AppColors.main_color,
+        iconColor: AppColors.main_color,
+        title: ScrollingOverflowText(text: _technicalNameWithTranslationsStore.getTechnicalNames(sub_task_title_id)!, textStyle: TextStyle(fontSize: 19),),
+        key: widget.subTasks[widget.index].globalKey,
+        children: <Widget>[
+          _buildExpansionContent(),
+        ],
+      );
+    }
 
   Widget _buildAppExpansionTileWidgetWithCustomTheme() {
     return ListTileTheme(
@@ -70,7 +90,6 @@ class SubTaskWidgetState extends State<SubTaskWidget> with AutomaticKeepAliveCli
     );
   }
 
-
   Widget _buildRoundedExpansionTile() {
     return ClipRRect(
       borderRadius: Dimens.expansionTileBorderRadius,
@@ -79,8 +98,7 @@ class SubTaskWidgetState extends State<SubTaskWidget> with AutomaticKeepAliveCli
           shape: RoundedRectangleBorder(
             borderRadius: Dimens.expansionTileBorderRadius,
           ),
-          child: _buildAppExpansionTileWidgetWithCustomTheme()
-      ),
+          child: _buildAppExpansionTileWidgetWithCustomTheme()),
     );
   }
 
