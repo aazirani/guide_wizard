68ca0f841da6c389424a629f940f2600493a6bc6
Shayan Kebriti <shayankebriti@gmail.com>
2023-07-17 11:35:12 +0330

    add DataLoadHandler

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:boilerplate/constants/colors.dart';
 import 'package:boilerplate/constants/dimens.dart';
-import 'package:boilerplate/data/network/constants/endpoints.dart';
+import 'package:boilerplate/data/data_laod_handler.dart';
 import 'package:boilerplate/stores/current_step/current_step_store.dart';
 import 'package:boilerplate/stores/step/step_store.dart';
 import 'package:boilerplate/stores/technical_name/technical_name_with_translations_store.dart';
-import 'package:boilerplate/stores/updated_at_times/updated_at_times_store.dart';
 import 'package:boilerplate/widgets/compressed_tasklist_timeline/compressed_task_list_timeline.dart';
 import 'package:boilerplate/widgets/step_slider/step_slider_widget.dart';
 import 'package:boilerplate/widgets/step_timeline/step_timeline.dart';
@@ -15,7 +13,6 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
-import 'package:simple_fontellico_progress_dialog/simple_fontico_loading.dart';
 import 'package:boilerplate/stores/language/language_store.dart';
 import 'package:boilerplate/stores/data/data_store.dart';
 
@@ -34,10 +31,10 @@ class _HomeScreenState extends State<HomeScreen> {
   late StepStore _stepStore;
   late TechnicalNameWithTranslationsStore _technicalNameWithTranslationsStore;
   late LanguageStore _languageStore;
-  late UpdatedAtTimesStore _updatedAtTimesStore;
   late CurrentStepStore _currentStepStore;
   Map _source = {ConnectivityResult.none: false};
   final MyConnectivity _connectivity = MyConnectivity.instance;
+  late DataLoadHandler _dataLoadHandler;
 
   @override
   void didChangeDependencies() {
@@ -45,11 +42,10 @@ class _HomeScreenState extends State<HomeScreen> {
     // initializing stores
     _dataStore = Provider.of<DataStore>(context);
     _stepStore = Provider.of<StepStore>(context);
-    _technicalNameWithTranslationsStore =
-        Provider.of<TechnicalNameWithTranslationsStore>(context);
+    _technicalNameWithTranslationsStore = Provider.of<TechnicalNameWithTranslationsStore>(context);
     _languageStore = Provider.of<LanguageStore>(context);
-    _updatedAtTimesStore = Provider.of<UpdatedAtTimesStore>(context);
     _currentStepStore = Provider.of<CurrentStepStore>(context);
+    _dataLoadHandler = DataLoadHandler(context);
   }
 
   @override
@@ -65,143 +61,8 @@ class _HomeScreenState extends State<HomeScreen> {
           .getCurrentLan(_languageStore.language_id!);
       print(_languageStore.locale);
 
-      // Loading data (from datasource if data is downloaded before):
-      await _loadDataWithoutErrorHandling(context); // TODO: delete this one
-      // Checking whether there is an update:
-      await _checkForUpdate(context);
-      // Loading data (from datasource if data is downloaded before):
-      await _loadDataWithoutErrorHandling(context);
-
+      await _dataLoadHandler.loadDataAndCheckForUpdate();
     });
-  }
-
-  SimpleFontelicoProgressDialog? _dialog;
-
-  Future<bool> _canConnectToServer() async {
-    bool ActiveConnection = false;
-    try {
-      final result = await InternetAddress.lookup(Endpoints.baseUrl);
-      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-        setState(() {
-          ActiveConnection = true;
-        });
-      }
-    } on SocketException catch (_) {
-      setState(() {
-        ActiveConnection = false;
-      });
-    }
-    return ActiveConnection;
-  }
-
-  Future<bool> _isConnectedToInternet() async {
-    bool hasConnection;
-    switch (_source.keys.toList()[0]) {
-      case ConnectivityResult.mobile:
-      case ConnectivityResult.wifi:
-        hasConnection = true;
-        break;
-      case ConnectivityResult.none:
-      default:
-        hasConnection = false;
-    }
-    return hasConnection;
-  }
-
-  void _showNoInternetConnectionDialog(BuildContext context) {
-    _showAlertDialog(
-        context,
-        AppLocalizations.of(context).translate("no_internet_title"),
-        AppLocalizations.of(context).translate("no_internet_content"),
-        AppLocalizations.of(context).translate("no_internet_button_text"),
-        _loadDataAndShowLoadingDialog);
-  }
-
-  void _showCantConnectToServer(BuildContext context) {
-    _showAlertDialog(
-        context,
-        AppLocalizations.of(context).translate("unable_to_reach_server_title"),
-        AppLocalizations.of(context)
-            .translate("unable_to_reach_server_content"),
-        AppLocalizations.of(context)
-            .translate("unable_to_reach_server_button_text"),
-        _loadDataAndShowLoadingDialog);
-  }
-
-  void _showAlertDialog(
-      BuildContext context,
-      String title,
-      String content,
-      String buttonText,
-      void Function(BuildContext context) onPressedFunction) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: Text(content),
-          actions: <Widget>[
-            TextButton(
-              child: Text(buttonText),
-              onPressed: () {
-                Navigator.of(context).pop(); // close the dialog
-                onPressedFunction(context);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _loadDataAndShowLoadingDialog(BuildContext context) async {
-    _dialog ??= SimpleFontelicoProgressDialog(context: context);
-    _dialog!.show(
-        message: AppLocalizations.of(context).translate("loading_dialog_text"),
-        type: SimpleFontelicoProgressDialogType.normal,
-        horizontal: true,
-        width: 175.0,
-        height: 75.0,
-        hideText: false,
-        indicatorColor: AppColors.main_color);
-    if (await _isConnectedToInternet()) {
-      if (await _canConnectToServer()) {
-        await _dataStore.getSteps();
-        _dialog!.hide();
-      } else {
-        _dialog!.hide();
-        _showCantConnectToServer(context);
-      }
-    } else {
-      _dialog!.hide();
-      _showNoInternetConnectionDialog(context);
-    }
-  }
-
-  Future _checkForUpdate(BuildContext context) async {
-    if (!_updatedAtTimesStore.loading) {
-      await _updatedAtTimesStore.updateContentIfNeeded(); //TODO: handle network exception here
-    }
-  }
-
-  Future _loadDataWithoutErrorHandling(BuildContext context) async {
-    _dialog ??= SimpleFontelicoProgressDialog(context: context);
-    _dialog!.show(
-        message: AppLocalizations.of(context).translate("loading_dialog_text"),
-        type: SimpleFontelicoProgressDialogType.normal,
-        horizontal: true,
-        width: 175.0,
-        height: 75.0,
-        hideText: false,
-        indicatorColor: AppColors.main_color);
-    if (!_dataStore.stepLoading) {
-      await _technicalNameWithTranslationsStore.getTechnicalNameWithTranslations();
-      await _dataStore.getSteps();
-      await _currentStepStore.setStepsCount(_dataStore.stepList.steps.length);
-      await _dataStore.getAllTasks();
-      await _dataStore.getQuestions();
-    }
-    _dialog!.hide();
   }
 
   @override
@@ -316,35 +177,4 @@ class _HomeScreenState extends State<HomeScreen> {
     _connectivity.disposeStream();
     super.dispose();
   }
-}
-
-class MyConnectivity {
-  MyConnectivity._();
-
-  static final _instance = MyConnectivity._();
-  static MyConnectivity get instance => _instance;
-  final _connectivity = Connectivity();
-  final _controller = StreamController.broadcast();
-  Stream get myStream => _controller.stream;
-
-  void initialise() async {
-    ConnectivityResult result = await _connectivity.checkConnectivity();
-    _checkStatus(result);
-    _connectivity.onConnectivityChanged.listen((result) {
-      _checkStatus(result);
-    });
-  }
-
-  void _checkStatus(ConnectivityResult result) async {
-    bool isOnline = false;
-    try {
-      final result = await InternetAddress.lookup(Endpoints.baseUrl);
-      isOnline = result.isNotEmpty && result[0].rawAddress.isNotEmpty;
-    } on SocketException catch (_) {
-      isOnline = false;
-    }
-    _controller.sink.add({result: isOnline});
-  }
-
-  void disposeStream() => _controller.close();
 }
