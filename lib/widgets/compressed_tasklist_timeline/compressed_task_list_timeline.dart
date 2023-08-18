a1107b00330914a52dcb63290c1b46d831273e48
Shayan Kebriti <shayankebriti@gmail.com>
2023-08-18 15:18:53 +0330

    Fix and Modify Home in progress tasks and questions description

--- lib/widgets/compressed_tasklist_timeline/compressed_task_list_timeline.dart
+++ lib/widgets/compressed_tasklist_timeline/compressed_task_list_timeline.dart
@@ -26,6 +26,10 @@ class _CompressedTasklistTimelineState
   late StepStore _stepStore;
   late TechnicalNameWithTranslationsStore _technicalNameWithTranslationsStore;
 
+  // Getters ............................................................
+  get _getScreenHeight => MediaQuery.of(context).size.height;
+  get _getScreenWidth => MediaQuery.of(context).size.width;
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -38,45 +42,34 @@ class _CompressedTasklistTimelineState
 
   @override
   Widget build(BuildContext context) {
-    return _buildTimelineContainer();
-  }
-
-  Widget _buildTimelineContainer() {
-    return Container(
-      padding: Dimens.timelineContainerPadding,
-      height: _getScreenHeight() / 2.8,
-      width: double.infinity,
-      child: Align(
-        alignment: Alignment.topLeft,
-        child: _buildTimeline(),
-      ),
-    );
+    return _buildTimeline();
   }
 
   Widget _buildTimeline() {
     return Observer(
-      builder: (_) => Align(
-        alignment: Alignment.topLeft,
+      builder: (_) => RawScrollbar(
         child: Timeline.tileBuilder(
-            theme: TimelineThemeData(
-              direction: Axis.vertical,
-              nodePosition: Dimens.timelineNodePosition,
-            ),
-            builder: TimelineTileBuilder(
-              itemCount: _dataStore.getNumberOfSteps() == 0
-                  ? 0
-                  : _dataStore.getNumberOfTasksFromAStep((_stepStore.currentStep) - 1),
-              itemExtent: 70,
-              contentsBuilder: (context, index) =>
-                  _buildContents(index, _stepStore),
-              indicatorBuilder: (context, index) => _buildIndicator(index, _stepStore.currentStep),
-              startConnectorBuilder: (context, index) => index == 0 && (_stepStore.currentStep) - 1 == 1
-                  ? Container()
-                  : _buildConnector(),
-              endConnectorBuilder: (context, index) => (_stepStore.currentStep)  == _dataStore.getNumberOfSteps()
-                  ? Container()
-                  : _buildConnector(),
-            )),
+          shrinkWrap: true,
+          // physics: AlwaysScrollableScrollPhysics(),
+          theme: TimelineThemeData(
+            direction: Axis.vertical,
+            nodePosition: Dimens.timelineNodePosition,
+          ),
+          builder: TimelineTileBuilder(
+            itemCount: _dataStore.getNumberOfSteps() == 0
+                ? 0
+                : _dataStore.getNumberOfTasksFromAStep((_stepStore.currentStep) - 1),
+            itemExtent: Dimens.compressedTaskListTimeLineItemExtend,
+            contentsBuilder: (context, index) => _buildContents(index, _stepStore),
+            indicatorBuilder: (context, index) => _buildIndicator(index, _stepStore.currentStep),
+            startConnectorBuilder: (context, index) => index == 0 && (_stepStore.currentStep) - 1 == 1
+                ? Container()
+                : _buildConnector(),
+            endConnectorBuilder: (context, index) => (_stepStore.currentStep)  == _dataStore.getNumberOfSteps()
+                ? Container()
+                : _buildConnector(),
+          ),
+        ),
       ),
     );
   }
@@ -102,44 +95,33 @@ class _CompressedTasklistTimelineState
 
   Widget _buildContents(index, stepStore) {
     return Container(
-      width: _getScreenWidth() / 1.23,
-      height: _getScreenHeight() / 15,
       margin: Dimens.contentLeftMargin,
       padding: Dimens.compressedTaskListContentPadding,
       decoration: BoxDecoration(
         color: AppColors.timelineCompressedContainerColor,
         borderRadius: BorderRadius.all(Radius.circular(Dimens.contentRadius)),
       ),
-      child: Row(
-        children: [
-          _buildContentTitle(index, stepStore),
-        ],
-      ),
+      child: _buildContentTitle(index, stepStore),
     );
   }
 
   Widget _buildContentTitle(index, stepStore) {
     var stepTitleId = _dataStore.getTaskTitleId(stepStore.currentStep - 1, index);
-    return Flexible(
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-            "${_technicalNameWithTranslationsStore.getTranslation(stepTitleId)}",
-            style: TextStyle(
-              color: AppColors.main_color,
-              fontSize: Dimens.taskListTimeLineContentTitle,
-            )),
-      ),
+    return Row(
+      children: [
+        Flexible(
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "${_technicalNameWithTranslationsStore.getTranslation(stepTitleId)}",
+              style: TextStyle(
+                color: AppColors.main_color,
+                fontSize: Dimens.taskListTimeLineContentTitle,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
-
-  Widget _buildContentMoreButton() {
-    return Align(
-        alignment: Alignment.centerRight,
-        child: Icon(Icons.more_vert, color: AppColors.main_color));
-  }
-
-  //general methods ............................................................
-  double _getScreenHeight() => MediaQuery.of(context).size.height;
-  double _getScreenWidth() => MediaQuery.of(context).size.width;
 }
